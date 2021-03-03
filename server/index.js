6d2d5dd9422cffb76beb2f0946635b142332df06
justinjeong <justin.jeong5@gmail.com>
2021-03-03 19:39:17 +0900

    add(server): Union type

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const mutations = require('./typedefs-resolvers/_mutations')
 const enums = require('./typedefs-resolvers/_enums')
 const equipments = require('./typedefs-resolvers/equipments')
 const supplies = require('./typedefs-resolvers/supplies')
+const softwares = require('./typedefs-resolvers/softwares')
+const givens = require('./typedefs-resolvers/givens')
 
 const typeDefs = [
   queries,
@@ -12,11 +14,15 @@ const typeDefs = [
   enums,
   equipments.typeDefs,
   supplies.typeDefs,
+  softwares.typeDefs,
+  givens.typeDefs,
 ]
 
 const resolvers = [
   equipments.resolvers,
   supplies.resolvers,
+  softwares.resolvers,
+  givens.resolvers,
 ]
 
 const server = new ApolloServer({ typeDefs, resolvers })
