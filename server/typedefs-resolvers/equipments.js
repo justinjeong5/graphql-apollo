a65daff72ff6c1393aa269f9e34c9aafbef98018
justinjeong <justin.jeong5@gmail.com>
2021-03-03 19:23:43 +0900

    add(server): EquipmentAdvs
    for various scalar type

--- server/typedefs-resolvers/equipments.js
+++ server/typedefs-resolvers/equipments.js
@@ -9,10 +9,25 @@ const typeDefs = gql`
     count: Int
     new_or_used: String
   }
+  type EquipmentAdv {
+    id: ID!
+    used_by: String!
+    count: Int!
+    use_rate: Float
+    is_new: Boolean!
+  }
 `
 const resolvers = {
   Query: {
     equipments: (parent, args) => dbWorks.getEquipments(args),
+    equipmentAdvs: (parent, args) => dbWorks.getEquipments(args)
+      .map((equipment) => {
+        if (equipment.used_by === 'developer') {
+          equipment.use_rate = Math.random().toFixed(2)
+        }
+        equipment.is_new = equipment.new_or_used === 'new'
+        return equipment
+      }),
   },
   Mutation: {
     deleteEquipment: (parent, args) => dbWorks.deleteItem('equipment', args),
