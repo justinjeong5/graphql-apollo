c90b4fe26bb9adc9a0f38e8b0ab5ca35e2b42aa3
justinjeong <justin.jeong5@gmail.com>
2021-03-03 18:17:52 +0900

    add(server): mutation - delete

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const typeDefs = gql`
     equipments: [Equipment]
     supplies: [Supply]
   }
+  type Mutation {
+    deleteEquipment(id: String): Equipment
+  }
   type Team {
     id: Int
     manager: String
@@ -40,6 +43,15 @@ const resolvers = {
       .filter((team) => (team.id === args.id))[0],
     equipments: () => database.equipments,
     supplies: () => database.supplies,
+  },
+  Mutation: {
+    deleteEquipment: (parent, args, context, info) => {
+      const deleted = database.equipments
+        .filter((equipment) => equipment.id === args.id)[0]
+      database.equipments = database.equipments
+        .filter((equipment) => equipment.id !== args.id)
+      return deleted
+    },
   }
 }
 const server = new ApolloServer({ typeDefs, resolvers })
